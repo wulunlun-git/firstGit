8772760e7a93508f3d4feaee5441466253a8fd66
wulunlun-git <13035056780@163.com>
2020-12-01 22:21:35 +0800

    主干分支

--- src/main/java/org/git/test01.java
+++ src/main/java/org/git/test01.java
@@ -2,6 +2,7 @@ package org.git;
 
 public class test01 {
     public static void main(String[] args) {
+        System.out.println("分支");
         System.out.println("test01");
         System.out.println("test02");
     }
