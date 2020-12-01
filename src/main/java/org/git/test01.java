5fe402e360dff3db3e2e4bb2df70d04df0f11e65
wulunlun-git <13035056780@163.com>
2020-12-01 22:24:00 +0800

    分支修改

--- src/main/java/org/git/test01.java
+++ src/main/java/org/git/test01.java
@@ -2,8 +2,6 @@ package org.git;
 
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("分支11");
-        System.out.println("test01");
-        System.out.println("test02");
+        System.out.println("分支修改");
     }
 }
